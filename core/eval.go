7d0affbed0f494fdbf6b496912cb4591c1c26499
Anurag <kr.anurag24@gmail.com>
2026-05-30 14:07:42 +0530

    get, set ttl

--- core/eval.go
+++ core/eval.go
@@ -3,7 +3,11 @@ package core
 import (
 	"errors"
 	"io"
+	"strconv"
+	"time"
 )
+
+var RESP_NIL []byte = []byte("$-1\r\n")
 
 func evalPING(args []string, c io.ReadWriter) error {
 	var b []byte
@@ -22,10 +26,99 @@ func evalPING(args []string, c io.ReadWriter) error {
 	return err
 }
 
+func evalSET(args []string, c io.ReadWriter) error {
+	if (len(args)) <= 1 {
+		return errors.New("(error) wrong number of args")
+	}
+
+	var exDurationMs int64 = -1
+
+	key, value := args[0], args[1]
+
+	for i := 2; i < len(args); i++ {
+		switch args[i] {
+		case "EX", "ex":
+			i++
+			if i == len(args) {
+				return errors.New("syntax error")
+			}
+
+			exDurationSec, err := strconv.ParseInt(args[3], 10, 64)
+			if err != nil {
+				return errors.New("ex value is not an integer or out of range")
+			}
+			exDurationMs = exDurationSec * 1000
+		default:
+			return errors.New("syntax error")
+		}
+	}
+
+	Put(key, NewObj(value, exDurationMs))
+	c.Write([]byte("+OK\r\n"))
+	return nil
+}
+
+func evalGET(args []string, c io.ReadWriter) error {
+	if len(args) != 1 {
+		return errors.New("wrong number of args for the get cmd")
+	}
+
+	var key string = args[0]
+	value := Get(key)
+
+	if value == nil {
+		c.Write(RESP_NIL)
+		return nil
+	}
+
+	if value.ExpiresAt != -1 && time.Now().UnixMilli() > value.ExpiresAt {
+		c.Write(RESP_NIL)
+		return nil
+	}
+
+	c.Write(Encode(value.Value, false))
+	return nil
+}
+
+func evalTTL(args []string, c io.ReadWriter) error {
+	if len(args) != 1 {
+		return errors.New("wrong number of args for the ttl cmd")
+	}
+
+	var key string = args[1]
+
+	obj := Get(key)
+
+	if obj == nil {
+		c.Write([]byte(":-2\r\n"))
+		return nil
+	}
+
+	if obj.ExpiresAt == -1 {
+		c.Write([]byte(":-1\r\n"))
+		return nil
+	}
+
+	timeRem := obj.ExpiresAt - time.Now().UnixMilli()
+	if timeRem < 0 {
+		c.Write([]byte(":-2\r\n"))
+		return nil
+	}
+
+	c.Write(Encode(int64(timeRem/1000), false))
+	return nil
+}
+
 func EvalAndRespond(cmd *RedisCmd, c io.ReadWriter) error {
 	switch cmd.Cmd {
 	case "PING":
 		return evalPING(cmd.Args, c)
+	case "SET":
+		return evalSET(cmd.Args, c)
+	case "GET":
+		return evalGET(cmd.Args, c)
+	case "TTL":
+		return evalTTL(cmd.Args, c)
 	default:
 		return evalPING(cmd.Args, c)
 	}
